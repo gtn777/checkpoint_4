0865f7727bf8c4c3995a18a477491c2f0959df9a
gtn777 <gtn777@orange.fr>
2020-07-29 13:45:00 +0200

    cors

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,11 +6,16 @@ const blogRouter = require('./blog.routes');
 const userRouter = require('./user.routes');
 const authRouter = require('./auth.routes');
 
+const cors = require('cors');
+const corsOptions = {
+    origin: process.env.CLIENT_APP_URL,
+};
+router.use(cors(corsOptions));
+
 router.use('/block', blockRouter);
 router.use('/article', articleRouter);
 router.use('/blog', blogRouter);
 router.use('/user', userRouter);
 router.use('/auth', authRouter);
-
 
 module.exports = router;
